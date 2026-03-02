b46ab4089a88f5aff448fecc8f6c4db285b40d2d
slimane-msb <slimanemesbah0@gmail.com>
2026-03-02 20:31:55 +0100

    code optimization

---
+++ makefile
@@ -0,0 +1,26 @@
+
+NPM = npm
+VITE = npx vite
+
+all: install dev
+
+install:
+	$(NPM) install
+
+dev:
+	$(VITE) --open
+
+build:
+	$(NPM) run build
+
+preview:
+	$(VITE) preview
+
+clean:
+	rm -rf node_modules
+	rm -rf dist
+	rm -rf .vite
+
+rebuild: clean install dev
+
+.PHONY: all install dev build preview clean rebuild
--- src/main copy.ts
+++ src/main copy.ts
@@ -1,3 +1,23 @@
+// // Export all utilities
+// export * from './utils/distance';
+// export { loadGraphAsync, load_city_graph } from './utils/graph_loader';
+
+// // Export shortest path algorithms
+// export * from './algorithms/shortest path/dijkstra';
+// export * from './algorithms/shortest path/astar';
+// export * from './algorithms/shortest path/greedyBestFirstSearch';
+// export * from './algorithms/shortest path/chosen_algo';
+
+// // Export TSP algorithms
+// export * from './algorithms/TSP/greedy_insertion';
+// export * from './algorithms/TSP/held_karp';
+// export * from './algorithms/TSP/lin_kernighan_heuristic';
+// export * from './algorithms/TSP/metaheuristics';
+// export * from './algorithms/TSP/nearest_neighbor_twoOpt';
+// export * from './algorithms/TSP/TwoOpt_Lin_Kernighan';
+// export * from './algorithms/TSP/chosen_tsp';
+
+
 import L from 'leaflet';
 import 'leaflet-routing-machine';
 // Import the Leaflet CSS directly into the TS file
